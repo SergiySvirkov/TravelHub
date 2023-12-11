2a6ed94e640298b7f551907cb559addfeb35c429
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-12-11 11:11:06 +0200

    Create Booking.js

---
+++ client/src/pages/Booking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Booking = () => {
+  // Implement the booking process and form components
+  return (
+    <div>
+      <h2>Booking</h2>
+      {/* Booking form and process components */}
+    </div>
+  );
+};
+
+export default Booking;
